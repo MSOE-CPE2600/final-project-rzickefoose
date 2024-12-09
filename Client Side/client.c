8984130f78fa1ad5ad4605c00c2ba7fc005f0fb5
Rose Zickefoose <zickefoosej@msoe.edu>
2024-12-09 13:19:34 -0600

    Created Files to Seperate Client and Server

---
+++ Client Side/client.c
@@ -0,0 +1,8 @@
+/***************************************************************** 
+* Filename: client.c
+* Description: Client side of the chatroom program
+* Author: Rose Zickefoose
+* Date: 12/9/24
+* Note (Compile Instructions): make
+*****************************************************************/
+
---
+++ Server Side/server.c
@@ -0,0 +1,8 @@
+/***************************************************************** 
+* Filename: server.c
+* Description: Server side of the chatroom program
+* Author: Rose Zickefoose
+* Date: 12/9/24
+* Note (Compile Instructions): make
+*****************************************************************/
+
